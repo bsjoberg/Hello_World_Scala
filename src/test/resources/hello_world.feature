e599834c92fc6531add77ac07d9c8e0996ac396f
bsjoberg <brian.sjoberg@excella.com>
2019-10-14 06:22:54 -0400

    Initial feature for Hello World and cukes runner

---
+++ src/test/resources/hello_world.feature
@@ -0,0 +1,5 @@
+Feature: Hello world
+  Scenario: Make sure my dev environment is working
+    Given I have a hello world app
+    When I say hi
+    Then I hear "Hello World"
---
+++ src/test/scala/RunCukesTest.scala
@@ -0,0 +1,7 @@
+
+import _root_.org.junit.runner.RunWith
+import io.cucumber.junit.{Cucumber, CucumberOptions}
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(strict=true)
+class RunCukesTest
